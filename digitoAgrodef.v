6cccbbc971e02e9ca2e4bed3ede0ea880622a440
oLeozito <143969738+Lenoom@users.noreply.github.com>
2024-07-24 21:32:46 -0300

    Digito do Agrodefensivo que fiz na UEFS

--- digitoAgrodef.v
+++ digitoAgrodef.v
@@ -1,25 +1,54 @@
-module digitoAgrodef(clock,sinal,botao,segmentos);
-	input clock,sinal,botao;
+module digitoAgrodef(clock,botaoAgro,estado,sinal,segmentos,pisca,agroDef);
+	input clock,botaoAgro,sinal;
+	input [1:0]estado;
 	output [6:0]segmentos; // a = 0 g = 6
-	wire [2:0]Q;
-	wire flag;
-	
-	or(flag,sinal,botao);
-	contadorseis(clock,botao,Q);// Voltar pra Flag
-	
+	output pisca,agroDef;
+	wire [2:0]Q,ff;
+	wire [6:0] segments1, segments2,aux;
+	wire agroDef;
+    
+	and(ff[0],botaoAgro,!estado[1],estado[0]);
+	and(ff[1],!sinal,estado[1],estado[0]);
+	and(aux[2],estado[1],estado[0],sinal);
+ 	and(aux[3],ff[0],botaoAgro);
+	or(guarda,aux[3],aux[2]);
+    
+	FlipFlopT(ff[0],ff[1],0,guarda,agroDef);
+    
+	and(pisca,agroDef,!botaoAgro,clock);
+    
+    
+	contadorseis(clock,botao,Q);
+    
 	// A
-	and(segmentos[0],Q[0],!Q[1],Q[2]);
+	and(segments1[0],Q[0],!Q[1],Q[2],agroDef);
 	// B
-	and(segmentos[1],!Q[0],!Q[1],!Q[2]);
+	and(segments1[1],!Q[0],!Q[1],!Q[2],agroDef);
 	// C
-	and(segmentos[2],Q[0],!Q[1],!Q[2]);
+	and(segments1[2],Q[0],!Q[1],!Q[2],agroDef);
 	// D
-	and(segmentos[3],!Q[0],Q[1],!Q[2]);
+	and(segments1[3],!Q[0],Q[1],!Q[2],agroDef);
 	// E
-	and(segmentos[4],Q[0],Q[1],!Q[2]);
+	and(segments1[4],Q[0],Q[1],!Q[2],agroDef);
 	// F
-	and(segmentos[5],!Q[0],!Q[1],Q[2]);
+	and(segments1[5],!Q[0],!Q[1],Q[2],agroDef);
 	// G
-	or(segmentos[6],Q[1],!Q[1]);
-	
+	or(aux[5],Q[1],!Q[1]);
+ 	and(segmentos[6],aux[5],agroDef);
+    
+	or(aux[4],Q[1],!Q[1]);
+ 	and(segments2[0],aux[4],!agroDef);
+ 	and(segments2[1],aux[4],!agroDef);
+ 	and(segments2[2],aux[4],!agroDef);
+ 	and(segments2[3],aux[4],!agroDef);
+ 	and(segments2[4],aux[4],!agroDef);
+ 	and(segments2[5],aux[4],!agroDef);
+    
+	or(segmentos[0],segments1[0],segments2[0]);
+	or(segmentos[1],segments1[1],segments2[1]);
+	or(segmentos[2],segments1[2],segments2[2]);
+	or(segmentos[3],segments1[3],segments2[3]);
+	or(segmentos[4],segments1[4],segments2[4]);
+	or(segmentos[5],segments1[5],segments2[5]);
+    
 endmodule
